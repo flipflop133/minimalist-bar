793d4fc59baa871da7b01ee70ba0fdf2c2bf5f0f
flipflop133 <francois.bechet@tutanota.com>
2023-12-20 21:36:07 +0100

    update build workflow

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+BUILD_DIR="build"
+BUILD_TYPE="Release" # Default build type
+
+# Check if the build type argument is provided
+if [ "$#" -gt 0 ]; then
+    BUILD_TYPE="$1"
+fi
+
+# Check if the build directory exists
+if [ ! -d "$BUILD_DIR" ]; then
+    echo "Creating $BUILD_DIR directory..."
+    mkdir "$BUILD_DIR" || exit 1
+fi
+
+cd "$BUILD_DIR" || exit 1
+
+echo "Build type: $BUILD_TYPE"
+
+# Run CMake with the specified build type
+cmake -DCMAKE_BUILD_TYPE="$BUILD_TYPE" ..
+
+# Build the project
+make
